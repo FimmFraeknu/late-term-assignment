c4faa3cb696f6ca7bec6ff5ebf819606e5e8aa34
jonagnar <JonnXor@gmail.com>
2014-10-30 23:35:14 +0000

    Imported Junit for unit test classes. Added default test cases.

--- tests/ArtificialPlayerTests.java
+++ tests/ArtificialPlayerTests.java
@@ -1,4 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class ArtificialPlayerTests {
 
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
 }
--- tests/PlayerOneTests.java
+++ tests/PlayerOneTests.java
@@ -1,4 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class PlayerTests {
 
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
 }
