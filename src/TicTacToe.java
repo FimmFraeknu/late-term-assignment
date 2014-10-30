6f386158e728e9a7fb925737096e03787dc952a4
Ragnar Borgþór Ragnarsson <ragnarbr1992@gmail.com>
2014-10-30 22:43:42 +0000

    Added functions for TicTacToe class. No functionality yet.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,18 @@
 
 public class TicTacToe {
-
+    //Athugar hvort allir retir séu teknir
+	public boolean BoardFull() {
+		return false;
+	}
+	//Allir reitir teknir/leikur unninn
+	public boolean GameOver() {
+		return false;
+	}
+	void DisplayBoard() {
+		
+	}
+	//Reynir að merkja reit fyrir þennan player.
+	boolean ChooseBox(int Player, RowVal row, int col) { 
+		return false; //int Player verður væntanlega klasi, setti int svo compile-ist
+	}
 }
