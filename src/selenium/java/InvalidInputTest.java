ab2a0fad12f0a7883bd195a4d704ce556f136a88
antonva <antona13@ru.is>
2014-11-02 19:01:22 +0000

    more selenium test majiggers

--- src/selenium/java/InvalidInputTest.java
+++ src/selenium/java/InvalidInputTest.java
@@ -11,29 +11,22 @@ import static org.junit.Assert.*;
 import java.util.regex.Pattern;
 import static org.apache.commons.lang3.StringUtils.join;
 
-public class DrawTest {
+public class InvalidInputTest {
 	private Selenium selenium;
 
 	@Before
 	public void setUp() throws Exception {
 		WebDriver driver = new FirefoxDriver();
-		String baseUrl = "http://tic-taco.herokuapp.com/";
+		String baseUrl = "http://46.149.26.108:4567/";
 		selenium = new WebDriverBackedSelenium(driver, baseUrl);
 	}
 
 	@Test
-	public void testDraw() throws Exception {
+	public void testInvalidInput() throws Exception {
 		selenium.refresh();
 		selenium.clickAt("//tr[2]/td[2]", "");
-		selenium.clickAt("css=td", "");
-		selenium.clickAt("//td[3]", "");
-		selenium.clickAt("//tr[3]/td", "");
-		selenium.clickAt("//tr[2]/td", "");
-		selenium.clickAt("//tr[2]/td[3]", "");
-		selenium.clickAt("//td[2]", "");
-		selenium.clickAt("//tr[3]/td[2]", "");
-		selenium.clickAt("//tr[3]/td[3]", "");
-		verifyEquals("Draw", selenium.getText("id=output"));
+		selenium.clickAt("//tr[2]/td[2]", "");
+		assertEquals("Invalid move.", selenium.getText("id=output"));
 	}
 
 	@After
